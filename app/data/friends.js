f383cbf10a1e1f0588cc5a4138392a74b22a4482
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-07-22 20:51:53 -0400

    Added additional styling. Added logic to sort friend list and return best friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,31 @@
-var friendsArray = [];
+var friendsArray = [
+
+    {
+		name: "Human Male #7316",
+		photo: "", 
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Human Female #1812",
+		photo: "",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Human Male #9216",
+		photo: "",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Human Female #3342",
+		photo: "",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Xena",
+		photo: "",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
 
 
 module.exports = friendsArray;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,9 +11,41 @@ app.get("/api/friends", function (request, response) {
 });
 
 app.post("/api/friends", function (request, response) {
-    response.push(req.body);
-    res.json(true);
     
+    console.log(request.body.scores);
+
+    var user = request.body;
+
+    for(var i = 0; i < user.scores.length; i++) {
+
+        user.scores[i] = parseInt(user.scores[i]);
+    }
+    
+     var bestFriend = 0;
+     var minDifference = 40;
+    
+    
+
+     for(var i = 0; i < friends.length; i++) {
+
+        var sumDifference = 0;
+        for (var j = 0; j < friends[i].scores.length; j++) {
+            var difference = Math.abs(user.scores[j] - friends[i].scores[j])
+            sumDifference += difference;
+        }
+
+        if(sumDifference < minDifference) {
+            bestFriend = i;
+            minDifference = sumDifference;
+
+        }
+     }
+
+     friends.push(user);
+    // response.push(req.body);
+    // res.json(true);
+    
+    response.json(friends[bestFriend]);
 });
 
  } 
